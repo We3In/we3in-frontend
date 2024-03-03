fe310fce52c8826b345e5f82241f1cd45caa962b
edanur-caglayann <caglayanedanur23@gmail.com>
2024-03-03 17:31:04 +0300

    login-register design

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,15 @@
+import { SignIn } from '@/components/SignIn'
+import { Spotlight } from '@/components/ui/Spotlight'
 import React from 'react'
 
 export default function Login() {
   return (
-    <div>burası login</div>
+    <div className="w-full h-screen bg-background antialiased flex justify-center items-center dark:bg-grid-white/[0.03] bg-grid-black/[0.03]">
+    <Spotlight
+      className="-top-40 left-0 md:left-60 md:-top-20"
+      fill="white"
+    />
+      <SignIn />
+    </div>
   )
 }
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,16 @@
+import { SignUp } from '@/components/SignUp'
+import { SpotlightPreview } from '@/components/SpotlightPreview'
+import { Spotlight } from '@/components/ui/Spotlight'
 import React from 'react'
 
-export default function register (){
+export default function Login() {
   return (
-    <div>burası register sayfası</div>
-  );
+    <div className="w-full h-screen bg-background antialiased flex justify-center items-center dark:bg-grid-white/[0.03] bg-grid-black/[0.03]">
+    <Spotlight
+      className="-top-40 left-0 md:left-60 md:-top-20"
+      fill="white"
+    />
+      <SignUp />
+    </div>
+  )
 }
