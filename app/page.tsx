d5c90d98c9e8700a1b57f6f7bcaa2951290945db
edanur-caglayann <caglayanedanur23@gmail.com>
2024-03-03 17:31:20 +0300

    navbar design

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
 import { HomeSection } from "@/components/HomeSection";
+import { Navbar } from "@/components/Navbar";
+import { SignUp } from "@/components/SignUp";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-    {/* todo : navbar */}
+      <Navbar />
       <HomeSection/>
       <p>Bugra</p>
   </main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import plugin from "tailwindcss";
 const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
-
+const svgToDataUri = require("mini-svg-data-uri");
 
 const config = {
   content: [
@@ -23,6 +23,9 @@ const config = {
       },
     },
     extend: {
+      boxShadow: {
+        input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -105,6 +108,16 @@ const config = {
             transform: "translateY(-50%)",
           },
         },
+        spotlight: {
+          from: {
+            opacity: "0",
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          to: {
+            opacity: "1",
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
@@ -114,12 +127,35 @@ const config = {
         third: "moveInCircle 40s linear infinite",
         fourth: "moveHorizontal 40s ease infinite",
         fifth: "moveInCircle 20s ease infinite",
+        spotlight: "spotlight 2s ease .75s 1 forwards",
       },
     },
   },
   plugins: [
     require("tailwindcss-animate"),
-    addVariablesForColors
+    addVariablesForColors,
+    function ({ matchUtilities, theme }: any) {
+      matchUtilities(
+        {
+          "bg-grid": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-grid-small": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-dot": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
   ],
 } satisfies Config
 
